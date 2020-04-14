727104725a11ac214b7fe3e829686a43375f49f2
John Carter <johncarter352@gmail.com>
2020-04-14 05:10:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,32 +27,28 @@ class Song
     @@artists.uniq
   end
   
-  def self.gcount(gcount)
-  count = 0 
-    @@genre.each do |genre|
-     if genre == gcount
-       count += 1 
+def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+      genre_count[genre] += 1
+      else
+      genre_count[genre] = 1
+      end
     end
-  end
     genre_count
   end
-  
-  def self.acount(acount)
-  count = 0 
-    @@artists.each do |artist|
-      if artist == acount
-      count += 1 
-    end
-  end
-    artist_count
-  end
 
-def self.genre_count
-Hash[@@genres.collect { |gcount| [gcount, gcount(gcount)] }]
-  end
-  
-def self.genre_count
-Hash[@@artists.collect { |acount| [acount, acount(acount)] }]
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+      artist_count[artist] +=1
+      else
+      artist_count[artist] = 1
+      end
+    end
+    artist_count
   end
 end
     
